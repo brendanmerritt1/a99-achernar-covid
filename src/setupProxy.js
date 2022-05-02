94d30a2d271cddfdcff7ba81de97cb8ecb4e43f7
Brendan Merritt <brm2000@live.unc.edu>
2022-05-02 08:50:33 -0400

    heroku try 11

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(["/app/*", "/api/*"], { target: "http://localhost:5555/" })
+    createProxyMiddleware(["/app/*", "/api/*"], { target: "https://secret-ridge-44451.herokuapp.com/" })
   );
 };
